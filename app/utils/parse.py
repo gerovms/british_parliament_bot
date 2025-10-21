1c811de7cbbcaf39257bb066c2deacc2ede5ccc2
gerovms <ger.ov.m.s@yandex.ru>
2025-10-21 21:54:41 +0400

    Edited Redis realisation

--- app/utils/parse.py
+++ app/utils/parse.py
@@ -15,7 +15,7 @@ import redis.asyncio as aioredis
 from ..db.db import get_document, save_document
 from .constants import (BASE_NO_PESON_URL, DELAY_TIME, FIRST_MONTH_DAY,
                         ITEMS_PER_PAGE, LAST_MONTH_DAY, MAIN_URL, MONTHS,
-                        PERSON, REDIS_EX)
+                        PERSON)
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
