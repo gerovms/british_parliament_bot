a6f7c2f21ba171c1f84b0c95c3305fe22a981fc7
gerovms <ger.ov.m.s@yandex.ru>
2025-10-24 14:20:06 +0400

    Fixed redis cache

--- app/redis/redis_client.py
+++ app/redis/redis_client.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 load_dotenv(dotenv_path=BASE_DIR / ".env")
-load_dotenv()
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT"))
